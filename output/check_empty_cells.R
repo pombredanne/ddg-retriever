753acbd6120aaeaed56e2219d1df9c395a36dbdf
Sebastian Baltes <research@sbaltes.com>
2019-09-27 10:29:37 +0930

    Update results for training set

--- output/check_empty_cells.R
+++ output/check_empty_cells.R
@@ -12,7 +12,7 @@ library(data.table)
 
 training_set <- fread("training.csv", header=TRUE, sep=",", quote="\"", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"), stringsAsFactors=FALSE)
 nrow(training_set)
-# 47,681
+# 47,671
 
 length(which(is.na(training_set$url)))
 # 0
@@ -27,24 +27,21 @@ training_set[is.na(training_set$title)]$query
 # ...
 
 length(which(is.na(training_set$snippet)))
-# 4
+# 3
 
 training_set[is.na(training_set$snippet)]$query
 # ...
 
 
-testing_set <- fread("testingUpdated.csv", header=TRUE, sep=",", quote="\"", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"), stringsAsFactors=FALSE)
+testing_set <- fread("testing.csv", header=TRUE, sep=",", quote="\"", strip.white=TRUE, showProgress=TRUE, encoding="UTF-8", na.strings=c("", "null"), stringsAsFactors=FALSE)
 nrow(testing_set)
-# 47,678
+#
 
 length(which(is.na(testing_set$url)))
-# 0
+#
 
 length(which(is.na(testing_set$title)))
-# 0
+#
 
 length(which(is.na(testing_set$snippet)))
-# 296
-
-testing_set[is.na(testing_set$snippet)]$query
-# ...
+#
